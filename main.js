f3a1b20dbb4ad92a5e19e062cddecac047a51719
Evan Doyle <doyleevan182@gmail.com>
2020-04-15 16:13:49 -0400

    Add DNA base mutation functionality & CLGs/testing

--- main.js
+++ main.js
@@ -6,18 +6,46 @@ const returnRandBase = () => {
 
 // Returns a random single stand of DNA containing 15 bases
 const mockUpStrand = () => {
-  const newStrand = [];
+  const newStrand = []
   for (let i = 0; i < 15; i++) {
-    newStrand.push(returnRandBase());
+    newStrand.push(returnRandBase())
   }
-  return newStrand;
+  return newStrand
 };
 
-// Returns obj containing specimenNum & dna properties
+// Returns obj containing properties specimenNum & dna that
+// correspond w/ parameters provided
 const pAequorFactory = (specimenNum, dna) => {
   return {
     specimenNum: specimenNum,
-    dna: [dna]
+    dna: dna,
+    mutate() {
+      console.log(`Mutating specimen ${this.specimenNum}, with bases: ${this.dna}`);
+      
+      // Selects random index of base to change
+      const randIndex = Math.floor(Math.random() * this.dna.length);
+      console.log('DNA Base to change at index: ' + randIndex);
+      
+      // New random base to replace
+      let newRandBase = returnRandBase();
+      console.log('New random DNA base to insert: ' + newRandBase);
+      
+      // Runs loop until new random base is different from previous base
+      while (this.dna[randIndex] === newRandBase) {
+        console.log('The two DNA bases are equal... Generating new random base');
+        newRandBase = returnRandBase();
+      }
+      
+      // Replaces original base selection w/ new random base
+      this.dna[randIndex] = newRandBase;
+      return this.dna;
+      console.log(`Newly Mutated DNA Strand: ${this.dna}`);
+    }
   }
 };
-console.log(pAequorFactory(1, mockUpStrand()));
+
+// Creates sample object
+let pAequorSampleObj = pAequorFactory(1, mockUpStrand());
+
+// Logs test of .mutate() method on sample object
+console.log(pAequorSampleObj.mutate());
