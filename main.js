7e1d51d0a105bf8a8def116dfdcda4cae4bd45a7
Evan Doyle <doyleevan182@gmail.com>
2020-05-20 13:45:28 -0400

    Added ability to save pool of survivors with pAequorSurvivors array
    - 30 Survivors array functionality
    - Fixed syntax issues

--- main.js
+++ main.js
@@ -6,11 +6,11 @@ const returnRandBase = () => {
 
 // Returns a random single stand of DNA containing 15 bases
 const mockUpStrand = () => {
-  const newStrand = []
+  const newStrand = [];
   for (let i = 0; i < 15; i++) {
-    newStrand.push(returnRandBase())
+    newStrand.push(returnRandBase());
   }
-  return newStrand
+  return newStrand;
 };
 
 // Returns obj containing properties specimenNum & dna that
@@ -20,27 +20,31 @@ const pAequorFactory = (specimenNum, dna) => {
     specimenNum: specimenNum,
     dna: dna,
     mutate() {
-      console.log(`Mutating specimen ${this.specimenNum}, with bases: ${this.dna}`);
-      
+      console.log(
+        `Mutating specimen ${this.specimenNum}, with bases: ${this.dna}`
+      );
+
       // Selects random index of base to change
       const randIndex = Math.floor(Math.random() * this.dna.length);
       console.log('DNA Base to change at index: ' + randIndex);
-      
+
       // New random base to replace
       let newRandBase = returnRandBase();
       console.log('New random DNA base to insert: ' + newRandBase);
-      
+
       // Runs loop until new random base is different from previous base
       while (this.dna[randIndex] === newRandBase) {
-        console.log('The two DNA bases are equal... Generating new random base');
+        console.log(
+          'The two DNA bases are equal... Generating new random base'
+        );
         newRandBase = returnRandBase();
       }
-      
+
       // Replaces original base selection w/ new random base
       this.dna[randIndex] = newRandBase;
       return this.dna;
       console.log(`Newly Mutated DNA Strand: ${this.dna}`);
-    }, 
+    },
     compareDNA(otherOrganism) {
       const homogeneities = this.dna.reduce((acc, curr, idx, arr) => {
         // If two bases are equal, increase counter by one
@@ -58,19 +62,36 @@ const pAequorFactory = (specimenNum, dna) => {
       const percentTo2Decimals = percentDNAsim.toFixed(2);
 
       // Log commonality statement on two compared bases
-      console.log(`${this.specimenNum} and ${otherOrganism.specimenNum} have ${percentTo2Decimals}% DNA in common.`);
+      console.log(
+        `${this.specimenNum} and ${otherOrganism.specimenNum} have ${percentTo2Decimals}% DNA in common.`
+      );
     },
     // Returns true if at least sixty percent of DNA bases are 'C' OR 'G'
     willLikelySurvive() {
       // Use .filter() method to create new array containing only C/G elements
-      const cOrG = this.dna.filter(el => el === 'C' || el === 'G');
-      
+      const cOrG = this.dna.filter((el) => el === 'C' || el === 'G');
       // Divide length of newly filtered C/G array by length of original DNA array
       // If >= sixty percent, return true - If not, return false
       return cOrG.length / this.dna.length >= 0.6;
     },
-  }
+  };
 };
+
+const pAequorSurvivors = []; // Create array to store pAequor specimen that survive
+let idCounter = 1; // Create variable name for id counter (counter up until 30)
+
+// Create 30 instances of pAequor that can survive (.willLikelySurvive() returns true)
+while (pAequorSurvivors.length < 30) {
+  // Loop until pAequorSurvivors array includes 30 specimens
+  let newOrg = pAequorFactory(idCounter, mockUpStrand()); // Generate new organism
+  if (newOrg.willLikelySurvive()) {
+    // If .willLikelySurvies returns true, push newOrg to array
+    pAequorSurvivors.push(newOrg);
+  }
+  idCounter++; // Increase idCounter by 1 up until 30
+}
+
+console.log(pAequorSurvivors); // Log pAequorSurvivors array
 
 // Creates sample objects
 let pAequorSampleObj1 = pAequorFactory(1, mockUpStrand());
