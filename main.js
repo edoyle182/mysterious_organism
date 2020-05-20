5481b5c01d7f971276ac2e2017284f4acd5d3024
Evan Doyle <doyleevan182@gmail.com>
2020-05-20 14:20:24 -0400

    Syntax & commenting fixes

--- main.js
+++ main.js
@@ -13,12 +13,13 @@ const mockUpStrand = () => {
   return newStrand;
 };
 
-// Returns obj containing properties specimenNum & dna that
-// correspond w/ parameters provided
+// Factory function for creating many objects - 
+// Containing properties specimenNum & dna that correspond w/ parameters provided
 const pAequorFactory = (specimenNum, dna) => {
   return {
     specimenNum: specimenNum,
     dna: dna,
+    // Changes a random base in the mockup strand to a new random base
     mutate() {
       console.log(
         `Mutating specimen ${this.specimenNum}, with bases: ${this.dna}`
@@ -45,9 +46,10 @@ const pAequorFactory = (specimenNum, dna) => {
       return this.dna;
       console.log(`Newly Mutated DNA Strand: ${this.dna}`);
     },
+    
     compareDNA(otherOrganism) {
       const homogeneities = this.dna.reduce((acc, curr, idx, arr) => {
-        // If two bases are equal, increase counter by one
+        // If 2 bases are equal, increase counter by 1
         if (arr[idx] === otherOrganism.dna[idx]) {
           return acc + 1;
         } else {
@@ -58,15 +60,16 @@ const pAequorFactory = (specimenNum, dna) => {
       // Calculate % of common bases
       const percentDNAsim = (homogeneities / this.dna.length) * 100;
 
-      // Limit % in common to two decimal places
+      // Limit % in common to 2 decimal places
       const percentTo2Decimals = percentDNAsim.toFixed(2);
 
-      // Log commonality statement on two compared bases
+      // Log commonality statement on 2 compared bases
       console.log(
         `${this.specimenNum} and ${otherOrganism.specimenNum} have ${percentTo2Decimals}% DNA in common.`
       );
     },
-    // Returns true if at least sixty percent of DNA bases are 'C' OR 'G'
+    // Determines likelihood of P. aequor surviving 
+    // Returns true (higher chance of survival) if at least 60% of DNA bases are 'C' OR 'G'
     willLikelySurvive() {
       // Use .filter() method to create new array containing only C/G elements
       const cOrG = this.dna.filter((el) => el === 'C' || el === 'G');
@@ -78,14 +81,14 @@ const pAequorFactory = (specimenNum, dna) => {
 };
 
 const pAequorSurvivors = []; // Create array to store pAequor specimen that survive
-let idCounter = 1; // Create variable name for id counter (counter up until 30)
+let idCounter = 1; // Create variable idCounter (counter up until 30)
 
 // Create 30 instances of pAequor that can survive (.willLikelySurvive() returns true)
 while (pAequorSurvivors.length < 30) {
   // Loop until pAequorSurvivors array includes 30 specimens
   let newOrg = pAequorFactory(idCounter, mockUpStrand()); // Generate new organism
   if (newOrg.willLikelySurvive()) {
-    // If .willLikelySurvies returns true, push newOrg to array
+    // If .willLikelySurvive returns true, push newOrg to array
     pAequorSurvivors.push(newOrg);
   }
   idCounter++; // Increase idCounter by 1 up until 30
